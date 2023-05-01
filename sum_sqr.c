dcd25417301f7e961376e04c6ff82b3422127c29
Vulcanric <jneric49@gmail.com>
2023-05-01 08:17:21 -0700

    A function that calculates the sum of squares of a giving input of numbers

---
+++ sum_sqr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * sumsqr - calculate the sum of squares of a giving input of numbers
+ * @n: amount of numbers entered by the user
+ *
+ * Return: nothing.
+ */
+void sumsqr(int n)
+{
+
+}
---
+++ testfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - testing the function sumsqr
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	int container = 6;
+	sumsqr(container);
+	return (0);
+}
