1563766176307e3bca86d8d888524012ac11b5d0
Vulcanric <123538376+Vulcanric@users.noreply.github.com>
2023-05-24 04:32:54 +0100

    Update rev_string.c

--- rev_string.c
+++ rev_string.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include "main.h"
-#include <string.h>
 #include <unistd.h>
 
 int revStr(char *str)
 {
 	int i = 0;
 
-	i = i + strlen(str);
+	i = i + _strlen(str);
 	while (i >= 0)
 	{
 		write(1, &str[i], 1);
